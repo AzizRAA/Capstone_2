63cf783a467b47686e3de72b2ee478ab5a388eaa
Botlhale Setou <botlhale.setou@gmail.com>
2022-10-18 22:18:15 +0200

    Complete header/nav section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './styles.css';
 const header = require('./modules/header.js');
+const background = require('./modules/artwork/background.jpg');
 
 window.onload = () => {
+	const bg = new Image();
+	bg.src = background;
+	bg.style.position = 'absolute';
+	bg.style.top = 0;
+	bg.style.zIndex = -1;
+	bg.style.width = '100vw';
 	const body = document.querySelector('body');
-	body.style.backgroundColor = 'rgb(0, 0, 60, 0.2)';
-	
+	body.append(bg);
+
 	header();
 };
 
