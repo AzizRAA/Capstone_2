39d74be33d778c28247d5d971a378e765860d532
Azizbek Ruzmatov <azizbekruzmatovraa@gmail.com>
2022-10-19 22:59:35 +0300

    bg setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './styles.css';
 
-const body = document.querySelector('body');
-body.style.backgroundColor = 'skyblue';
+// const generateRGB = require('./modules/animations');
+const getRandomInt = require('./modules/animations');
+const setSmoothColorChange = require('./modules/animations');
+const renderEmoji = require('./modules/animations');
+const handleMouseMove = require('./modules/animations');
+const getMousePosition = require('./modules/animations');
+const anime = require('./modules/animations');
+const renderMarquee = require('./modules/api');
+
+window.onload = () => {
+  renderBackground();
+  renderHeader();
+  renderFooter();
+};
--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-	mode: 'development',
-	devServer: {
+  mode: 'development',
+  devServer: {
     static: './dist',
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-		clean: true,
+    clean: true,
   },
-	plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-			 	type: 'asset/resource',
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
-			},
-		],
-	},
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
