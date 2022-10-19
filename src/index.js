4d69733dd80dfed8ffc24e94a5ee272cff60bd82
Botlhale Setou <botlhale.setou@gmail.com>
2022-10-19 11:09:06 +0200

    Create background.js; export to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import './styles.css';
+
 const header = require('./modules/header.js');
-const background = require('./modules/artwork/background.jpg');
+const renderBackground = require('./modules/background.js');
 
 window.onload = () => {
-	const bg = new Image();
-	bg.src = background;
-	bg.style.position = 'absolute';
-	bg.style.top = 0;
-	bg.style.zIndex = -1;
-	bg.style.width = '100vw';
-	const body = document.querySelector('body');
-	body.append(bg);
-
+	renderBackground();
 	header();
 };
-
---
+++ src/modules/background.js
@@ -0,0 +1,15 @@
+const background = require('./artwork/background.jpg');
+
+renderBackground = () => {
+	const bg = new Image();
+	bg.src = background;
+	bg.style.position = 'absolute';
+	bg.style.top = 0;
+	bg.style.zIndex = -1;
+	bg.style.width = '100vw';
+	
+	const body = document.querySelector('body');
+	body.append(bg);
+};
+
+module.exports = renderBackground;
